3e858c27a1e1cd6a39828987828b65032292cb13
Lucas Trognon <lucas.trognon@arcanite.ch>
2021-11-24 16:14:33 +0100

    sync

--- helpers.py
+++ helpers.py
@@ -1,8 +1,10 @@
-def split_in_k(row,k, seed=1):
+import numpy as np
+def split_in_k(y,row,k, seed=1):
     chunk_size = len(row[0])//k
     print("chunk size : {}".format(chunk_size))
     np.random.seed(seed)
     indices = np.random.permutation(len(row[0]))
     
     for i in range(k):
-        yield [row[0][indices[i * chunk_size: (i + 1) * chunk_size]], row[1][indices[i * chunk_size: (i + 1) * chunk_size]]] 
+        index=indices[i * chunk_size: (i + 1) * chunk_size]
+        yield y[index],[row[0][index], row[1][index]] 
