519b4064f3d0352a94509b0ead93600a8c563c82
Lucas Trognon <lucas.trognon@arcanite.ch>
2021-12-02 15:15:44 +0100

    Grid search on unique vs double bits

--- helpers.py
+++ helpers.py
@@ -1,10 +1,11 @@
 import numpy as np
 def split_in_k(y,row,k, seed=1):
-    chunk_size = len(row[0])//k
-    print("chunk size : {}".format(chunk_size))
+    chunk_size = len(row)//k
     np.random.seed(seed)
-    indices = np.random.permutation(len(row[0]))
+    indices = np.random.permutation(len(row))
     
     for i in range(k):
         index=indices[i * chunk_size: (i + 1) * chunk_size]
-        yield y[index],[row[0][index], row[1][index]] 
+
+
+        yield (y,row[index])
