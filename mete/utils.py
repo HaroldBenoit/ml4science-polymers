00f7cc5ecca4d24ddc62136d5eab73a83fde51b0
Mete Ismayil <mismayilza@gmail.com>
2021-12-03 02:16:53 +0100

    some more experiments

--- mete/utils.py
+++ mete/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.signal import find_peaks_cwt
 
 def find_extrema(event, diff_threshold=2):
     extrema = []
@@ -43,9 +44,11 @@ def plot_data(data, plot_extrema=False, diff_threshold=2):
         g.set_ylabel('Current')
 
         if plot_extrema:
-            extrema = find_extrema(event, diff_threshold=diff_threshold)
-            time = extrema[:, 0]
-            current = extrema[:, 1]
+            # extrema = find_extrema(event, diff_threshold=diff_threshold)
+            peaks_idx = find_peaks_cwt(event[:, 1], widths=event[-1][0])
+            peaks = event[peaks_idx]
+            time = peaks[:, 0]
+            current = peaks[:, 1]
             g = sns.scatterplot(x=time, y=current, ax=axes[i], s=100, color='blue')
 
 def build_features(event, diff_threshold=0):
@@ -56,8 +59,17 @@ def build_features(event, diff_threshold=0):
         'min_current': 0, 
         'mean_current': 0,
         'std_current': 0,
-        'num_extrema': 0, 
-        'mean_extrema_diff': 0
+        # 'num_extrema': 0, 
+        # 'mean_extrema': 0,
+        # 'std_extrema': 0,
+        # 'mean_extrema_diff': 0,
+        'num_peaks': 0,
+        'mean_peaks': 0,
+        'peak_1': 0,
+        'peak_2': 0,
+        'peak_3': 0,
+        'peak_4': 0,
+        'peak_5': 0
     }
     if len(event) > 0:
         features['num_signals'] = len(event)
@@ -66,7 +78,19 @@ def build_features(event, diff_threshold=0):
         features['min_current'] = np.min(event[:, 1])
         features['mean_current'] = np.mean(event[:, 1])
         features['std_current'] = np.std(event[:, 1])
-        extrema = find_extrema(event, diff_threshold=diff_threshold)
-        features['num_extrema'] = len(extrema)
-        features['mean_extrema_diff'] = np.mean(np.abs([extrema[i, 1] - extrema[i-1, 1] for i in range(1, len(extrema))]))
+        # extrema = find_extrema(event, diff_threshold=diff_threshold)
+        # features['num_extrema'] = len(extrema)
+        # features['mean_extrema'] = np.mean(extrema)
+        # features['std_extrema'] = np.std(extrema)
+        # features['mean_extrema_diff'] = np.mean(np.abs([extrema[i, 1] - extrema[i-1, 1] for i in range(1, len(extrema))]))
+        peaks_idx = find_peaks_cwt(event[:, 1], widths=max([1, event[-1][0]]))
+        features['num_peaks'] = len(peaks_idx)
+        if len(peaks_idx) > 0:
+            peaks = sorted(event[peaks_idx][:, 1], reverse=True)
+            features['mean_peaks'] = np.mean(peaks)
+            features['peak_1'] = peaks[0] if len(peaks) > 0 else 0
+            features['peak_2'] = peaks[1] if len(peaks) > 1 else 0
+            features['peak_3'] = peaks[2] if len(peaks) > 2 else 0
+            features['peak_4'] = peaks[3] if len(peaks) > 3 else 0
+            features['peak_5'] = peaks[4] if len(peaks) > 4 else 0
     return features
