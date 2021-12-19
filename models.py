fa4e1684e4c16769d8a8aa5aac7d4cfb4e8354f7
Mete Ismayil <mismayilza@gmail.com>
2021-12-19 02:07:39 +0100

    add run function

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
 import numpy as np
+from collections import namedtuple
+from itertools import product
 import torch
 from torch.utils.data import DataLoader, Subset
 import torch.nn as nn
 import torch.nn.functional as F
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 import wandb
+
+from pipeline import AABB245_Pipeline, PolymerDataset, train_test_split
+
+
+Parameter = namedtuple('Parameter', ['name', 'value'])
+
 
 class VanillaLSTM(nn.Module):
 
@@ -72,24 +80,19 @@ class MultiOutputLSTM(nn.Module):
         return preds
 
 
-def compute_accuracy(model, dataset):
-    loader = DataLoader(dataset, batch_size=len(dataset))
-    X, y = next(iter(loader))
-    preds = model.predict(X)
-    accuracy = (preds == y).sum() * 100 / len(y)
-    return accuracy
-
-
 def train(train_dataset, model, test_dataset=None, num_epochs=100, batch_size=512, weight_decay=0.001, lr_rate=0.001, verbose=2):
-    whole_dataset = train_dataset.dataset if isinstance(train_dataset, Subset) else train_dataset
+    full_dataset = train_dataset.dataset if isinstance(train_dataset, Subset) else train_dataset
     config = dict(
-        **whole_dataset.info(),
+        **full_dataset.info(),
+        **model.info(),
         num_epochs=num_epochs,
         batch_size=batch_size,
         weight_decay=weight_decay,
         learning_rate=lr_rate,
         optimizer='Adam',
-        loss='NLLLoss'
+        loss='NLLLoss',
+        train_size=len(train_dataset),
+        test_size=len(test_dataset) if test_dataset is not None else None
     )
     wandb.init(project="ml4science-polymers", config=config)
 
@@ -110,24 +113,28 @@ def train(train_dataset, model, test_dataset=None, num_epochs=100, batch_size=51
             preds = torch.argmax(probs, dim=1, keepdim=False)
             num_correct += (preds == y).sum()
             losses.append(loss.item())
-        
-        if verbose > 1 or (verbose > 0 and epoch % 50 == 0) or epoch == num_epochs-1:
-            avg_loss = np.mean(losses)
-            accuracy = num_correct*100/len(train_dataset)
-            wandb.log({'train_loss': avg_loss, 'train_accuracy': accuracy})
+
+        avg_loss = np.mean(losses)
+        accuracy = num_correct*100/len(train_dataset)
+        wandb.log({'train_loss': avg_loss, 'train_accuracy': accuracy})
+
+        if verbose > 1 or (verbose > 0 and (epoch % 50 == 0 or epoch == num_epochs-1)):
             print(f'epoch={epoch}/{num_epochs}, loss={np.mean(losses)}, accuracy={accuracy}')
 
+    test_metrics = {}
+
     if test_dataset is not None:
-        test_scores = test(test_dataset, model)
-        print(f"test scores={test_scores}")
-        wandb.log({f"test_{name.lower()}": score for name, score in test_scores.items()})
+        test_metrics = test(test_dataset, model)
+        wandb.log({f"test_{name.lower()}": metric for name, metric in test_metrics.items()})
 
-    wandb.finish()
+    wandb.finish(quiet=True)
 
-    return model
+    return model, test_metrics
 
 
-def test(dataset, model, batch_size=1024):
+def test(dataset, model, batch_size=4096):
+    full_dataset = dataset.dataset if isinstance(dataset, Subset) else dataset
+    multiclass = full_dataset.num_classes > 2
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     predictions = np.array([])
     labels = np.array([])
@@ -138,13 +145,40 @@ def test(dataset, model, batch_size=1024):
             predictions = np.concatenate((predictions, preds), axis=None)
             labels = np.concatenate((labels, y), axis=None)
 
-    names = ["Accuracy", "F1 Score", "Precision", "Recall"]
-    functions = [accuracy_score, f1_score, precision_score, recall_score]
-
-    scores = {}
-
-    for name, func in zip(names,functions):
-        score = func(labels, predictions)
-        scores[name] = score
+    metrics = {
+        'accuracy': accuracy_score(labels, predictions),
+        'f1_score': f1_score(labels, predictions, average='weighted' if multiclass else 'binary'),
+        'precision': precision_score(labels, predictions, average='weighted' if multiclass else 'binary'),
+        'recall': recall_score(labels, predictions, average='weighted' if multiclass else 'binary'),
+        'confusion_matrix': confusion_matrix(labels, predictions, normalize='true').round(3)
+    }
     
-    return scores
+    return metrics
+
+
+def run_245(param_grid):
+    parameter_space = []
+
+    for param, values in param_grid.items():
+        parameters = []
+
+        if not isinstance(values, list) and not isinstance(values, np.ndarray):
+            values = [values]
+
+        for value in values:
+            parameters.append(Parameter(name=param, value=value))
+
+        parameter_space.append(parameters)
+
+    for params in product(*parameter_space):
+        params_dict = {param.name: param.value for param in params}
+    
+        pipeline = AABB245_Pipeline(num_blocks=params_dict.get('num_blocks', 8), extrema_th=params_dict.get('extrema_th', 8))
+        dataset = PolymerDataset(params_dict['data_paths'], pipeline)
+        model = MultiOutputLSTM(input_dim=dataset.num_features, output_dim=dataset.num_classes, num_blocks=dataset.num_blocks)
+        train_data, test_data = train_test_split(dataset)
+        model, metrics = train(train_dataset=train_data, test_dataset=test_data, model=model,
+                               batch_size=params_dict.get('batch_size', 512), num_epochs=params_dict.get('num_epochs', 100),
+                               lr_rate=params_dict.get('lr_rate', 0.001),
+                               weight_decay=params_dict.get('weight_decay', 0.001),
+                               verbose=0)
